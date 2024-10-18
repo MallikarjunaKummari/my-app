7d1ad5c212d1558f6c2421e1cb43302e5fe07be8
Mallikarjuna <mallikarjunakummari2003@gmail.com>
2024-10-18 14:55:01 +0530

    hhhh

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>mallikarjuna</h1>
+        <h1>MalliKarjuna</h1>
       </header>
     </div>
   );
